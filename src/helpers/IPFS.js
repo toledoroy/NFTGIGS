5eddb576b3978c0ff6217ad875249555b5e147ae
Roy <toledoroy@gmail.com>
2022-02-04 07:31:56 -0800

    Added some helpers

---
+++ src/helpers/IPFS.js
@@ -0,0 +1,78 @@
+//Export IPFS Helper
+export const IPFS = {
+  //IPFS Gateway Servers
+  gateway: {
+    moralis: "https://ipfs.moralis.io:2053/ipfs/",
+    cloudflare: "https://cloudflare-ipfs.com/ipfs/",
+  },
+
+  // resolveLink(url){ return (!url || !url.includes("ipfs://")) ? url : url.replace("ipfs://", "https://gateway.ipfs.io/ipfs/"); },
+  // resolveLink(url){ return (!url || !url.includes("ipfs://")) ? url : url.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/"); },
+  resolveLink(url) {
+    return !url || !url.includes("ipfs://")
+      ? url
+      : url.replace("ipfs://", "https://ipfs.moralis.io:2053/ipfs/");
+  }, //Use Moralis on Client
+  resolveLinkCloudflare(url) {
+    return !url || !url.includes("ipfs://")
+      ? url
+      : url.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/");
+  },
+
+  /** ...Needs Moralis...
+   * Save JSON File to IPFS
+   */
+  saveJSONToIPFS: async (Moralis, jsonObject, fileName = "file.json") => {
+    //Save Metadata to IPFS
+    const file = new Moralis.File(fileName, {
+      base64: btoa(JSON.stringify(jsonObject)),
+    });
+    return file.saveIPFS().then((result) => {
+      console.log("[DEV] IPFS.saveJSONToIPFS() IPFS Hash for Metadata:", {
+        // result,
+        hash: result.hash(),
+        hash2: file.hash(),
+        // jsonObject,
+      });
+      //Return IPFS URL
+      // return result.ipfs();    //Moralis URL
+      return "ipfs://" + result.hash(); //General IPFS Conventional URL
+    });
+    // .catch(function(error) { console.error("[CAUGHT] IPFS.saveJSONToIPFS() IPFS Call Failed:", {error, user:Moralis.User.current() }); });
+  }, //saveJSONToIPFS()
+
+  /**
+   * Save Image file to IPFS
+   * @var object image
+   * @ret string imageFile
+   * @ret String URL
+   */
+  saveImageToIPFS: async (Moralis, image, fileName = "image.png") => {
+    // const data = fileInput.files[0]
+    // const file = new Moralis.File(data.name, data)
+    const file = new Moralis.File(fileName, image);
+
+    //Save File to Object
+    // const jobApplication = new Moralis.Object('Applications')
+    // jobApplication.set('resume', file)
+
+    //Save Image to IPFS & Return Parse File
+    // return await file.saveIPFS();
+    await file.saveIPFS();
+    let url = "ipfs://" + file.hash(); //General IPFS Conventional URL
+    return url;
+  }, //saveImageToIPFS()
+
+  /**
+   * Fetch File From IPFS by Hash (Works with JSON Filed)
+   * @param string ipfsHash
+   * @returns object
+   */
+  fetchJSONFromIPFS: async (ipfsHash) => {
+    const url = `https://ipfs.moralis.io:2053/ipfs/${ipfsHash}`; //Force Moralis Gateway
+    const response = await fetch(url);
+    return await response.json();
+  },
+};
+
+export default IPFS;
---
+++ src/helpers/__.js
@@ -0,0 +1,69 @@
+//Export General Helper
+export const __ = {
+  sanitize: (str) => {
+    try {
+      return typeof str === "string" ? str.toString().replace(/_/g, " ") : str;
+    } catch (err) {
+      console.error("__.sanitize() Error sanitizing metadata field:", {
+        str,
+        err,
+      });
+      return str;
+    }
+  }, //sanitize()
+
+  nl2br: (str) => {
+    return !str ? str : str.replace(/(?:\r\n|\r|\n)/g, "<br/>");
+  },
+
+  stripHTML: (str) => {
+    return !str ? str : str.replace(/<[^>]*>?/gm, "");
+  },
+
+  stackContainerStyle: (length) => {
+    let style = {};
+    if (length && length > 1) {
+      let len = length < 4 ? length : 4; //Max of 4 in a stack
+      style.transform = "rotate(-" + 5 * (len - 1) + "deg)";
+    }
+    return style;
+  },
+
+  matchAddr: (a1, a2) => String(a1).toLowerCase() === String(a2).toLowerCase(),
+
+  /**
+   * Match URI Function
+   * Try to ignore similar IPFS URLs
+   * @param string uri1
+   * @param string uri2
+   * @returns
+   */
+  matchURI: (uri1, uri2) => {
+    if (String(uri1).toLowerCase().includes("ipfs")) {
+      //Try to ignore this if URLs have the same IPFS ID
+      let uri1Adjusted = uri1
+        .replace("https://ipfs.moralis.io:2053/ipfs/", "")
+        .replace("ipfs://", "");
+      let uri2Adjusted = uri2
+        .replace("https://ipfs.moralis.io:2053/ipfs/", "")
+        .replace("ipfs://", "");
+
+      // if(uri1Adjusted !== uri2Adjusted) console.warn("[TEST] Different IPFS IDs", {uri1, uri2, uri1Adjusted, uri2Adjusted});   //V
+
+      return uri1Adjusted === uri2Adjusted;
+    }
+    return uri1 === uri2;
+  },
+
+  /**
+   * Chop string to size & Add '...' at the end
+   * @param {string} str
+   * @param {number} n    Max number of chars
+   * @returns {string}
+   */
+  ellipsis: (str, n = 6) => {
+    return str.length > n ? `${str.trim().slice(0, n)}...` : str;
+  },
+};
+
+export default __;
