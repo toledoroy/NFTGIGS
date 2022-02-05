4870f966507f8bc768de63775d7112b638126f14
Roy <toledoroy@gmail.com>
2022-02-05 11:03:37 -0800

    Store Contract Details in a Context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 import {
@@ -25,6 +26,7 @@ import Text from "antd/lib/typography/Text";
 import Ramper from "components/Ramper";
 import Offers from "components/Offer/Offers";
 import MenuItems from "./components/MenuItems";
+import { OfferContractContext } from "context/context";
 const { Header, Footer } = Layout;
 
 const styles = {
@@ -68,121 +70,139 @@ const App = ({ isServerInfo }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated, isWeb3Enabled]);
 
+  // Offers Contract
+
+  //Offers v.0.1.3 on Mumbai
+  // const contractData = {
+  //   chain: "mumbai",
+  //   hash: "0x46e5BAbAd693DBb352002652f660508c65515969",
+  // abi: require('contracts/abi/Offers.json'),
+  // };
+  //TEST CONTRACT
+  const contractData = {
+    //Something on Polygon
+    chain: "polygon",
+    hash: "0xe93a85fc751513b99feead66a9d29a83a8704c71",
+  };
+  contractData.abi = require("contracts/abi/Offers.json");
+
   return (
-    <Layout style={{ height: "100vh", overflow: "auto" }}>
-      <Router>
-        <Header style={styles.header}>
-          <Logo />
-          <MenuItems />
-          <div style={styles.headerRight}>
-            <Chains />
-            {/* <TokenPrice
+    <OfferContractContext.Provider value={{ contractData }}>
+      <Layout style={{ height: "100vh", overflow: "auto" }}>
+        <Router>
+          <Header style={styles.header}>
+            <Logo />
+            <MenuItems />
+            <div style={styles.headerRight}>
+              <Chains />
+              {/* <TokenPrice
               address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
               chain="polygon"
               image="https://cloudflare-ipfs.com/ipfs/QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg/"
               size="40px"
             /> */}
-            <NativeBalance />
-            <Account />
+              <NativeBalance />
+              <Account />
+            </div>
+          </Header>
+
+          <div style={styles.content}>
+            <Switch>
+              <Route exact path="/quickstart">
+                <QuickStart isServerInfo={isServerInfo} />
+              </Route>
+              <Route path="/wallet">
+                <Wallet />
+              </Route>
+              <Route path="/1inch">
+                <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
+                  <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
+                    <DEX chain="eth" />
+                  </Tabs.TabPane>
+                  <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
+                    <DEX chain="bsc" />
+                  </Tabs.TabPane>
+                  <Tabs.TabPane tab={<span>Polygon</span>} key="3">
+                    <DEX chain="polygon" />
+                  </Tabs.TabPane>
+                </Tabs>
+              </Route>
+              <Route path="/erc20balance">
+                <ERC20Balance />
+              </Route>
+              <Route path="/onramp">
+                <Ramper />
+              </Route>
+              <Route path="/erc20transfers">
+                <ERC20Transfers />
+              </Route>
+              <Route path="/offers">
+                <Offers />
+              </Route>
+              <Route path="/nftBalance">
+                <NFTBalance />
+              </Route>
+              <Route path="/contract">
+                <Contract />
+              </Route>
+              <Route path="/">
+                <Redirect to="/quickstart" />
+              </Route>
+              <Route path="/ethereum-boilerplate">
+                <Redirect to="/quickstart" />
+              </Route>
+              <Route path="/nonauthenticated">
+                <>Please login using the "Authenticate" button</>
+              </Route>
+            </Switch>
           </div>
-        </Header>
+        </Router>
+        <Footer style={{ textAlign: "center" }}>
+          <Text style={{ display: "block" }}>
+            ⭐️ Please star this{" "}
+            <a
+              href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              boilerplate
+            </a>
+            , every star makes us very happy!
+          </Text>
 
-        <div style={styles.content}>
-          <Switch>
-            <Route exact path="/quickstart">
-              <QuickStart isServerInfo={isServerInfo} />
-            </Route>
-            <Route path="/wallet">
-              <Wallet />
-            </Route>
-            <Route path="/1inch">
-              <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
-                <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
-                  <DEX chain="eth" />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
-                  <DEX chain="bsc" />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
-                  <DEX chain="polygon" />
-                </Tabs.TabPane>
-              </Tabs>
-            </Route>
-            <Route path="/erc20balance">
-              <ERC20Balance />
-            </Route>
-            <Route path="/onramp">
-              <Ramper />
-            </Route>
-            <Route path="/erc20transfers">
-              <ERC20Transfers />
-            </Route>
-            <Route path="/offers">
-              <Offers />
-            </Route>
-            <Route path="/nftBalance">
-              <NFTBalance />
-            </Route>
-            <Route path="/contract">
-              <Contract />
-            </Route>
-            <Route path="/">
-              <Redirect to="/quickstart" />
-            </Route>
-            <Route path="/ethereum-boilerplate">
-              <Redirect to="/quickstart" />
-            </Route>
-            <Route path="/nonauthenticated">
-              <>Please login using the "Authenticate" button</>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-      <Footer style={{ textAlign: "center" }}>
-        <Text style={{ display: "block" }}>
-          ⭐️ Please star this{" "}
-          <a
-            href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            boilerplate
-          </a>
-          , every star makes us very happy!
-        </Text>
+          <Text style={{ display: "block" }}>
+            🙋 You have questions? Ask them on the {""}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
+            >
+              Moralis forum
+            </a>
+          </Text>
 
-        <Text style={{ display: "block" }}>
-          🙋 You have questions? Ask them on the {""}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
-          >
-            Moralis forum
-          </a>
-        </Text>
-
-        <Text style={{ display: "block" }}>
-          📖 Read more about{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat"
-          >
-            Moralis
-          </a>
-        </Text>
-        <Text style={{ display: "block" }}>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://faucet.polygon.technology/"
-          >
-            Polygon Faucet
-          </a>
-        </Text>
-      </Footer>
-    </Layout>
+          <Text style={{ display: "block" }}>
+            📖 Read more about{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat"
+            >
+              Moralis
+            </a>
+          </Text>
+          <Text style={{ display: "block" }}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://faucet.polygon.technology/"
+            >
+              Polygon Faucet
+            </a>
+          </Text>
+        </Footer>
+      </Layout>
+    </OfferContractContext.Provider>
   );
 };
 
