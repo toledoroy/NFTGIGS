154b180b09e901ad2f6537ed0eab56fb1480ec3f
Roy <toledoroy@gmail.com>
2022-02-05 18:38:58 -0800

    [WIP] Prep Order Pages

---
+++ src/components/OrderSingle.jsx
@@ -0,0 +1,13 @@
+
+/**
+ * My Orders Page
+ */
+function OrderSingle(props) {
+    return (
+        <div className="framed offer">
+            <h1>Order 00</h1>
+        </div>
+    );
+} //OrderSingle()
+
+export default OrderSingle;
---
+++ src/components/Orders.jsx
@@ -0,0 +1,13 @@
+
+/**
+ * My Orders Page
+ */
+function Orders(props) {
+    return (
+        <div className="framed offer">
+            <h1>My Orders</h1>
+        </div>
+    );
+} //Orders()
+
+export default Orders;
